3228b01af503e06697fb7589f472a6b565238518
Elena Granados <patty518@yahoo.com>
2018-11-10 15:16:00 -0800

    create login and registration validation

---
+++ public/client.js
@@ -0,0 +1,157 @@
+'use strict';
+
+$('.signup-button').click(event => {
+    // event.preventDefault();
+    console.log("got to sign up");
+    signupForm();
+});
+
+$('.signup-anchor').click(event => {
+    event.preventDefault();
+    console.log("got to sign up");
+    signupForm();
+});
+
+function signupForm() { 
+    $('.intro').hide();
+    $('.signup-form').removeClass('hidden');
+    if(!($('.login-form').hasClass('hidden'))) {
+        $('.login-form').addClass('hidden');
+    };
+};
+
+$('.login-button').click(event => {
+    // event.preventDefault();
+    console.log("got to login");
+    $('.intro').hide();
+    $('.login-form').removeClass('hidden');
+    if(!($('.signup-form').hasClass('hidden'))) {
+        $('.signup-form').addClass('hidden');
+    }
+});
+
+$('.login-anchor').click(event => {
+    console.log("got to login");
+    $('.intro').hide();
+    $('.login-form').removeClass('hidden');
+    if(!($('.signup-form').hasClass('hidden'))) {
+        $('.signup-form').addClass('hidden');
+    }
+});
+
+
+
+// API calls to users router
+
+// sign up API call to create user
+$('.signup-form').submit(function(event) {
+    event.preventDefault();
+    console.log("signup form ran");
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const username = $('#username').val();
+    const password = $('#password').val();
+    const match = $('#match-password').val();
+
+
+    //validate the input
+    if (firstName == "") {
+        alert('Please add a first name');
+    } else if (lastName == "") {
+        alert('Please add a last name');
+    } else if (username == "") {
+        alert('Please add an user name');
+    } else if (password == "") {
+        alert('Please add a password');
+    } else if (match == "") {
+        alert('Please confirm password');
+    } else if (password != match) {
+        alert('password and confirm password must match')
+    }
+    //if the input is valid
+    else {
+        //create the payload object (what data we send to the api call)
+        const newUserObject = {
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            password: password,
+            
+        };
+        console.log(newUserObject);
+
+        //make the api call using the payload above
+        $.ajax({
+                type: 'POST',
+                url: '/users/create',
+                dataType: 'json',
+                data: JSON.stringify(newUserObject),
+                contentType: 'application/json'
+            })
+            //if call is successfull
+            .done(function(result) {
+                $('#loggedInUserName').val(result.username);
+                $('.validation').addClass('hidden');
+                $('.top-menu').removeClass('hidden');
+                $('.search').removeClass('hidden');
+                
+            })
+            //if the call is failing
+            .fail(function(jqXHR, error, errorThrown) {
+                console.log(jqXHR);
+                console.log(error);
+                console.log(errorThrown);
+                const message = jqXHR.responseJSON.message;
+                alert(message);
+            });
+    }
+});
+
+// login API call
+$('.login-form').submit(function(event) {
+    event.preventDefault();
+    console.log("login form ran");
+
+    const username = $('#login-username').val();
+    const password = $('#login-password').val();
+    // console.log(username);
+    // console.log(password);
+
+    if (username == "") {
+        alert('Please enter username');
+    } else if (password == "") {
+        alert('Please enter password');
+    } else {
+        //create the payload object (what data we send to the api call)
+        const userObject = {
+            username: username,
+            password: password
+        };
+        console.log(userObject);
+        //make the api call using the payload above
+        $.ajax({
+                type: 'POST',
+                url: '/users/login',
+                dataType: 'json',
+                data: JSON.stringify(userObject),
+                contentType: 'application/json'
+            })
+            //if call is successfull
+            .done(function(result) {
+                $('#loggedInUserName').val(result.username);
+                console.log(result.username);
+                $('.validation').addClass('hidden');
+                $('.top-menu').removeClass('hidden');
+                $('.search').removeClass('hidden');
+            })
+            // if the call is failing
+            .fail(function(jqXHR, error, errorThrown) {
+                console.log(jqXHR);
+                console.log(error);
+                console.log(errorThrown);
+                const message = jqXHR.responseJSON.message;
+                alert(message);
+            });
+
+    };
+});
