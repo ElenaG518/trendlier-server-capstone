cdd5ec70c81cd1bd0be52bcc42f549769c56cb99
Elena Granados <patty518@yahoo.com>
2018-12-08 21:59:43 -0800

    correct client_origin

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.CLIENT_ORIGIN= 'https://trendlier-client-capstone.herokuapp.com' || 'http://localhost:3000';
 
-exports.CLIENT_ORIGIN= process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+// exports.CLIENT_ORIGIN= process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 // exports.CLIENT_ORIGIN= process.env.CLIENT_ORIGIN ||'https://trendlier-client-capstone.herokuapp.com/'
 
