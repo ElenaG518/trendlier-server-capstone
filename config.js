60cb59e82254f8979844e25d52ff74b381dc2e2c
Elena Granados <patty518@yahoo.com>
2018-12-08 21:35:35 -0800

    heroku app config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://user:pas
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-// exports.CLIENT_ORIGIN= 'https://trendlier-client-capstone.herokuapp.com' || 'http://localhost:3000';
+exports.CLIENT_ORIGIN= 'https://trendlier-client-capstone.herokuapp.com' || 'http://localhost:3000';
 
 exports.CLIENT_ORIGIN= process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 // exports.CLIENT_ORIGIN= process.env.CLIENT_ORIGIN ||'https://trendlier-client-capstone.herokuapp.com/'
