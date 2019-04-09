da0561ded796276cd5e5544598dd11976783287d
Elena Granados <patty518@yahoo.com>
2019-04-09 01:03:01 -0700

    modify config.js

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 'use strict'
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user:pass123@ds045475.mlab.com:45475/trendlier-server-capstone';
-// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://user:pass123@ds045475.mlab.com:45475/trendlier-server-capstone';
 
 exports.DATABASE_URL = process.env.DATABASE_URL; 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
-// exports.CLIENT_ORIGIN= 'https://trendlier-client-capstone.herokuapp.com' || 'http://localhost:3000';
-
 exports.CLIENT_ORIGIN= process.env.CLIENT_ORIGIN || 'http://localhost:3000';
